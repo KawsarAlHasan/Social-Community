0e0afa67cf451bac93921a8030accfbdc33d19cd
mukarrom <mukarrom.hosain.1@gmail.com>
2022-07-20 22:21:06 +0600

    react router dom installed and navigation created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './pages/Share/Nav';
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>This is MERN Stuck Squad</h1>
+      <Nav/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/network' element={<Home/>}/>
+        <Route path='/jobs' element={<Home/>}/>
+        <Route path='/messaging' element={<Home/>}/>
+        <Route path='/notification' element={<Home/>}/>
+      </Routes>
     </div>
   );
 }
