9defc24b88d573958f4963014d8deed87898bd39
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2022-07-22 17:27:28 +0600

    added slidebar main and widgets of Home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is MERN Stuck Squad</h1>
+    <div className='bg-base-200'>
+      <Home />
     </div>
   );
 }
