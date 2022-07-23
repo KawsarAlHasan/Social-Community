92a3327dddba44a643d4c7a7027b00f3a6250db9
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2022-07-23 19:36:57 +0600

    added home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Nav from './Pages/Share/Nav';
 import {Routes, Route} from 'react-router-dom';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <div className="">
       <Nav/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/network' element={<Home/>}/>
+        <Route path='/jobs' element={<Home/>}/>
+        <Route path='/messaging' element={<Home/>}/>
+        <Route path='/notification' element={<Home/>}/>
+      </Routes>
     </div>
   );
 }
