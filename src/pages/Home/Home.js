4e5615eacc3e19d8136897913e9fb1264def3f59
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2022-07-28 03:19:06 +0600

    Responsive by Home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,12 +8,12 @@ const Home = (props) => {
     return (
         <div className='bg-base-200'>
             <div className="grid grid-cols-12">
-                <div className="col-span-3 mt-3 grid justify-items-end"> <Sidebar /> </div>
-                <div className="col-span-6 mt-3 justify-items-center">
+                <div className="lg:col-span-3 md:col-span-4 sm:col-span-12 mt-3 sm:justify-items-center grid md:justify-items-end"> <Sidebar /> </div>
+                <div className="lg:col-span-6 md:col-span-8 sm:col-span-12 mt-3 justify-items-center">
                     <Post />
                     <Card />
                 </div>
-                <div className="col-span-3 mt-3 grid justify-items-start"> <Widgets /> </div>
+                <div className="lg:col-span-3 sm:hidden lg:block mt-3 grid lg:justify-items-start"> <Widgets /> </div>
             </div>
         </div>
     );
