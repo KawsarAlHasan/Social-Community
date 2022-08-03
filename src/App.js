501b13a8882150a79254425c6acd5be9902540d6
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2022-08-04 01:47:45 +0600

    start messangers

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home/Home'
 import UserProfile from './pages/UserProfile/UserProfile'
+import NotFound from './pages/NotFound/NotFound'
+import Messages from './pages/Messages/Messages'
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/userProfile" element={<UserProfile />} />
+          <Route path="/messaging" element={<Messages />} />
+          <Route path="/*" element={<NotFound />} />
         </Routes>
       </Navbar>
     </div>
