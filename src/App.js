4c88f14576105f3a5573dd57c59bc664b6475f33
Nahid Ahmmed Likhon <lemonnahid19@gmail>
2022-08-05 13:16:20 +0600

    New

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './pages/Home/Home'
-import UserProfile from './pages/UserProfile/UserProfile'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home/Home";
+import UserProfile from "./pages/UserProfile/UserProfile";
+import MyNetwork from "./pages/MyNetwork/MyNetwork";
 
 function App() {
   return (
@@ -11,10 +12,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/userProfile" element={<UserProfile />} />
+          <Route path="/network" element={<MyNetwork />} />
         </Routes>
       </Navbar>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
